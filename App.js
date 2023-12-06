152af5f8f9f01b53143ff53dcd550801c75a706f
klaus <klausongudy@gmail.com>
2023-12-06 19:43:40 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View , Platform} from 'react-native';
+import Navigation from "./navigations/Navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
 
@@ -14,7 +12,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop : Platform.OS === 'android' ? StatusBar.currentHeights : 0,
   },
 });
